bf60d38a3a23e3588cb24ad6eb4cf3230ea7629e
Harshavardhan-karre <harshavardhan.karre@zemosolabs.com>
2024-08-17 23:17:04 +0530

    Feat - Adding drag and drop feature for menu items

--- JS_Restaurant/landpg.js
+++ JS_Restaurant/landpg.js
@@ -11,8 +11,7 @@ const elemobj = [
   { id: 4, name: "Shahi Paneer", price: "Rs.240" },
   { id: 5, name: "Ras Malai", price: "Rs.380" },
   { id: 6, name: "Veg Biryani", price: "Rs.280" },
-  { id: 7, name: "Aloo Gobi", price: "Rs.160" },
-  { id: 8, name: "Meals", price: "Rs.210" },
+  { id: 7, name: "Meals", price: "Rs.210" },
 ];
 elemobj.forEach((obj) => {
   const inel = document.createElement("div");
@@ -21,6 +20,10 @@ elemobj.forEach((obj) => {
   inel.innerHTML += `<h4>${obj.name}</h4><h4>${obj.price}</h4>`;
   elem.appendChild(inel);
   tracker.push({ ele: inel, i: obj });
+  inel.setAttribute("data-id", obj.id);
+  inel.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("text/plain", e.target.dataset.id);
+  });
 });
 menpg.appendChild(elem);
 
@@ -31,18 +34,41 @@ const tabtracker = [];
 const tables = document.createElement("div");
 tables.classList.add("tablist");
 const tabobj = [
-  { id: 1, name: "Table-1", price: "Rs.300", Total_Items: "2" },
+  { id: 1, name: "Table-1", price: "Rs.0", Total_Items: "0" },
   { id: 2, name: "Table-2", price: "Rs.0", Total_Items: "0" },
-  { id: 3, name: "Table-3", price: "Rs.999", Total_Items: "0" },
+  { id: 3, name: "Table-3", price: "Rs.0", Total_Items: "0" },
 ];
 tabobj.forEach((obj) => {
   const inel = document.createElement("div");
   inel.classList.add("tableitems");
   inel.innerHTML += `<h4>${obj.name}</h4><h4>${obj.price}</h4><h4>Total Items: ${obj.Total_Items}</h4>`;
   tables.appendChild(inel);
+  inel.setAttribute("data-id", obj.id);
   tabtracker.push({ ele: inel, i: obj });
+  inel.addEventListener("dragover", (e) => e.preventDefault());
+  inel.addEventListener("drop", (e) => {
+    e.preventDefault();
+    const itemid = e.dataTransfer.getData("text/plain");
+    console.log(itemid);
+    const menuitem = getdragelm(itemid);
+    console.log(menuitem);
+    const tableid = inel.dataset.id;
+    console.log(tableid);
+    // draggable.classList.remove("hide");
+    const tablelm = tabobj.find((x) => x.id == tableid);
+    console.log(tablelm);
+    updatetable(menuitem, tablelm);
+  });
 });
 tablepg.appendChild(tables);
+
+function getdragelm(id) {
+  for (let { ele, i } of tracker) {
+    if (i.id == id) {
+      return ele;
+    }
+  }
+}
 
 // Filtering items in Menu
 
@@ -114,4 +140,21 @@ function searchtable() {
   }
 }
 
-// Drag and drop Feature
+// Drop
+
+function updatetable(menuitem, tablelm) {
+  const menprice = menuitem.getElementsByTagName("h4");
+  const prc = menprice[1].textContent.slice(3);
+  const updprice = parseInt(tablelm.price.slice(3)) + parseInt(prc);
+  let reqd;
+  for (let { ele, i } of tabtracker) {
+    if (i.id == tablelm.id) {
+      reqd = ele;
+      break;
+    }
+  }
+  tablelm.price = "Rs." + updprice;
+  tablelm.Total_Items = parseInt(tablelm.Total_Items) + 1;
+  reqd.innerHTML = `<h4>${tablelm.name}</h4><h4>${tablelm.price}</h4><h4>Total Items: ${tablelm.Total_Items}</h4>`;
+  // console.log(reqd);
+}
