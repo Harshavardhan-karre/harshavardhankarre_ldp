1685d28d4483481332d8333feee8047989c0d122
Harshavardhan-karre <harshavardhan.karre@zemosolabs.com>
2024-10-09 16:11:05 +0530

    Feat-Adding java Assignment_5

---
+++ src/assignment_5/AssignmentPart3.java
@@ -0,0 +1,23 @@
+package assignment_5;
+
+
+/*
+Create main class in package yourname.assignment.main, create object of first class and call both the methods
+ to print the values. Create object of second class using static method and then call the other method
+ to print the String.
+ */
+public class AssignmentPart3 {
+
+    public static void main(String[] args) {
+
+        Assignment_5 as5 = new Assignment_5();
+        as5.printVariables();
+        as5.secondMethod();
+    }
+    static void staticLocalMethod()
+    {
+        AssignmentPart2 as2 = new AssignmentPart2();
+        as2.printText();
+    }
+
+}
